8be3021f778b0b88da4020c1a8398f83b5e0ccb6
Marianna <mariannacanchala451@gmail.com>
2025-07-09 17:32:46 -0500

    condicionales

---
+++ condicionales.py
@@ -0,0 +1,68 @@
+#--------------------------TALLER CONDICIONALES-----------------------------------
+
+#1 verifica si el numero es positivo o negativo
+
+# num1 = float (input("ingrese n numero: "))
+
+# if num1 > 0:
+#     print (f"positivo")
+# elif num1 < 0 :
+#     print (f"es negativo porque es {num1}")
+# else:
+#     print ("es cero")
+
+#2 calcula el mayor de los numeros ingresados
+
+# num1= float(input("ingresa el primer numero: "))
+# num2= float(input("ingresa el segundo numero: "))
+# if num1>num2:
+#     print(f"el numero {num1} es mayor que {num2}")
+# elif num2>num1:
+#     print(f"el numero {num2} es mayor que {num1}")    
+
+
+#3 determina si el numero es par o impar
+
+# numero= float (input("ingrese un numero"))
+# if numero == 0:
+#     print("El número es cero, que es considerado par.")
+# elif numero % 2 == 0:
+#     print("El número es par.")
+# else:
+#     print("El número es impar.")
+
+# #4 dado un numero, verifica si esta entre 10 y 20
+
+# numero = int(input("Ingresa un número: "))
+# if 10 <= numero <= 20:
+#     print("El número está entre 10 y 20.")
+# else:
+#     print("El número NO está entre 10 y 20.")
+
+#5 encuentra el mayor usando condicionales
+
+# num1 = float(input("Ingresa el primer número: "))
+# num
+# 2 = float(input("Ingresa el segundo número: "))
+# num3 = float(input("Ingresa el tercer número: "))
+# if num1 >= num2 and num1 >= num3:
+#     mayor = num1
+# elif num2 >= num1 and num2 >= num3:
+#     mayor = num2
+# else:
+#     mayor = num3
+# print("El número mayor es:", mayor)
+
+#6 calcula el precio final con un 10% de descuento si el total es mayor a $100
+
+# precios= float(input("que vale el tomate?: "))
+# if precios> 100:
+#     els= precios*0.10
+#     preciostotales= els - precios
+#     print(f"el precio final del producto con el 18% de descuenmto  de {precios}$ del tomate es {els}$")
+# else:
+#     print(f" el producto esta menos que $100 es un buen  precio y no nse le hara el desdduento")
+
+
+
+
